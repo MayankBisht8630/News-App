3b0c26d886fe8c21a1e32b44de9d82a0a4ef3d19
Mayank Bisht <mayank8630bisht@gmail.com>
2024-10-12 05:37:38 +0530

    updated repo

--- app/src/main/java/com/mayankbisht/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/mayankbisht/thenewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.mayankbisht.thenewsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "0e9899dc724a44ea80e83ef532fd72a0"
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
